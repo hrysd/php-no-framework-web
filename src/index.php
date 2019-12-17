462090808914ad8dea6978e2776b0bac0db3b535
Hiroshi Yoshida <hrysd22@gmail.com>
2019-12-17 13:58:22 +0900

    break

--- src/index.php
+++ src/index.php
@@ -14,8 +14,11 @@ $path = $_GET['path'];
 switch ($path) {
     case 'hoge':
         echo 'hoge';
+        break;
     case 'e_warning':
         echo WARNING;
+        break;
     default:
         echo 'Hey!';
+        break;
 }
